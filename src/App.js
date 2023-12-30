8261d2c87d6815be762eab0b617c9a9869ea50ec
shalini47ch <shalini47choudhary@gmail.com>
2023-12-30 14:59:45 +0530

    toggle added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Students from "./components/Students";
 
 function App() {
   const [name, updateName] = useState("shalini");
-  const[data,setData]=useState(null)
+  const [data, setData] = useState(null);
   const [print, setPrint] = useState(false);
+  //here we have implemented the functionality of hide as well as show using useState
+  const [status,setStatus]=useState(true);//so initially we will want to show the element so we will keep it as true
 
   function updateData() {
     updateName("Manisha");
@@ -17,14 +19,13 @@ function App() {
   //to show our data on our screen we can use useState hook of react
   function getData(e) {
     console.log(e.target.value);
-    setData(e.target.value)
+    setData(e.target.value);
   }
 
   return (
     <div className="App">
-      {
-        print?<h1>{data}</h1>:null
-      }
+      {print ? <h1>{data}</h1> : null}
+      {status?<h1>Hello</h1>:null}
       <User />
       <Customer />
       <button onClick={handleClick}>Click Me</button>
@@ -34,6 +35,9 @@ function App() {
       <Students name={name} age="24" />
       <input type="text" onChange={getData} />
       <button onClick={() => setPrint(true)}>Print Name</button>
+      <button onClick={()=>setStatus(false)}>Hide</button>
+      <button onClick={()=>setStatus(true)}>Show</button>
+      <button onClick={()=>setStatus(!status)}>Toggle</button>
     </div>
   );
 }
@@ -44,3 +48,6 @@ export default App;
 //Now we are updating the state of our react application using the use state
 
 //This dynamic update of names or rather data is possible by using the useState in React
+
+//Now applying the logic for hide and show to show or hide button content on the basis of button click
+//if we put the opposite of the existing state we can use that to toggle
